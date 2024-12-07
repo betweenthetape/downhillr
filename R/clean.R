6a5283faa3c3db891e7f7cd167bff4d613ded06e
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2024-12-07 18:11:08 +0100

    Extract logic for timed training into fun

--- R/clean.R
+++ R/clean.R
@@ -57,3 +57,75 @@ clean_results_table <- function(data) {
       split_2, split_3, split_4, time, time_from_leader, dnf, dsq, dns, points
     )
 }
+
+clean_timed_training_table <- function(data) {
+  data <- data |>
+    fill(Nr)
+
+  split_1 <- data |>
+    filter((row_number() - 1) %% 4 == 0) |>
+    rename(
+      rank = Rank,
+      nr = Nr,
+      name = `Name / UCI MTB Team`,
+      nat = NAT,
+      run_1_speed = Speed...5,
+      run_1_split_1 = Splits...6,
+      run_1_time = Time...7,
+      run_2_speed = Speed...8,
+      run_2_split_1 = Splits...9,
+      run_2_time = Time...10,
+      run_3_speed = Speed...11,
+      run_3_split_1 = Splits...12,
+      run_3_time = Time...13,
+      best_time = Time...14
+    )
+
+  split_2 <- data |>
+    filter((row_number() - 2) %% 4 == 0) |>
+    select(
+      nr = Nr,
+      uci_team = `Name / UCI MTB Team`,
+      run_1_split_2 = Splits...6,
+      run_2_split_2 = Splits...9,
+      run_3_split_2 = Splits...12,
+      best_time_from_leader = Time...14
+    )
+
+  split_3 <- data |>
+    filter((row_number() - 3) %% 4 == 0) |>
+    select(
+      nr = Nr,
+      run_1_split_3 = Splits...6,
+      run_2_split_3 = Splits...9,
+      run_3_split_3 = Splits...12,
+    )
+
+  split_4 <- data |>
+    filter((row_number() - 4) %% 4 == 0) |>
+    select(
+      nr = Nr,
+      run_1_split_4 = Splits...6,
+      run_2_split_4 = Splits...9,
+      run_3_split_4 = Splits...12,
+    )
+
+  list(split_1, split_2, split_3, split_4) |>
+    reduce(left_join) |>
+    mutate(
+      name = str_squish(str_remove(name, "\\*$")),
+      best_time_from_leader = str_remove(best_time_from_leader, "^\\+"),
+      across(everything(), ~ if_else(.x == "-", NA, .x)),
+      across(
+        c(starts_with("run_"), starts_with("best_time")),
+        ~ convert_to_seconds(.x)
+      ),
+      across(c(rank, nr), ~ as.numeric(.x))
+    ) |>
+    select(
+      rank, nr, name, uci_team, nat, run_1_speed, starts_with("run_1_split"),
+      run_1_time, run_2_speed, starts_with("run_2_split"), run_2_time,
+      run_3_speed, starts_with("run_3_split"), run_3_time, best_time,
+      best_time_from_leader
+    )
+}

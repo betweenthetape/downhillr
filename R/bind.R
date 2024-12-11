da9beb1439836c6e9b666d27111bfcfb38a58455
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2024-12-11 18:12:49 +0100

    Move bind function to utils

--- R/bind.R
+++
@@ -1,9 +0,0 @@
-#' Bind a list of tables extracted from a PDF.
-#'
-#' @param tables A list of data frames extracted from a PDF.
-#' @noRd
-bind_tables <- function(tables) {
-  tables |>
-    map(~ mutate(.x, across(everything(), as.character))) |>
-    list_rbind()
-}
--- R/utils.R
+++ R/utils.R
@@ -16,3 +16,13 @@ convert_to_seconds <- function(x) {
     }
   )
 }
+
+#' Bind a list of tables extracted from a PDF.
+#'
+#' @param tables A list of data frames extracted from a PDF.
+#' @noRd
+bind_tables <- function(tables) {
+  tables |>
+    map(~ mutate(.x, across(everything(), as.character))) |>
+    list_rbind()
+}

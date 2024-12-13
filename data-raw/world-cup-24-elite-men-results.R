c65e1e56a424595055bf41d2cb1da86c373ed47e
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2024-12-13 20:13:34 +0100

    Extract and export results

--- data-raw/world-cup-24-elite-men-results.R
+++ data-raw/world-cup-24-elite-men-results.R
@@ -5,13 +5,13 @@
 # ------------------------------------------------------------------------------
 
 # ---- Load ----
+pkgload::load_all()
 library(tidyverse)
 library(tabulapdf)
-pkgload::load_all()
 
 # ---- Create spec ----
 # Run `pdf_areas(<INSERT PATH>)` on each path to determine PDF table areas.
-results_spec <- tribble(
+spec <- tribble(
   ~path, ~areas, ~metadata,
   "inst/extdata/20240503-fwil_dhi_me_results_f.pdf",
   list(
@@ -30,10 +30,10 @@ results_spec <- tribble(
   ),
   "inst/extdata/20240503-fwil_dhi_me_results_qr.pdf",
   list(
-    c(134.3694, 19.3861, 742.4009, 575.0968),
-    c(133.5767, 20.1788, 42.4009, 575.8895),
-    c(133.5767, 19.3861, 743.1937, 575.8895),
-    c(133.5767, 20.1788, 602.086, 574.304)
+    c(133.57667, 20.57519, 743.19365, 575.49312),
+    c(133.57667, 20.57519, 743.19365, 575.49312),
+    c(133.57667, 19.78245, 743.19365, 575.49312),
+    c(133.57667, 20.57519, 603.67143, 576.28586)
   ),
   list(
     event_name = "Fort William",
@@ -347,122 +347,23 @@ results_spec <- tribble(
   )
 )
 
-timed_training_spec <- tribble(
-  ~path, ~areas, ~metadata,
-  "inst/extdata/20240503-fwil_dhi_me_results_tt.pdf",
-  list(
-    c(140.7113, 20.1788, 738.4372, 576.6822),
-    c(140.7113, 19.3861, 736.8517, 575.8895),
-    c(140.7113, 20.1788, 738.4372, 575.8895),
-    c(140.7113, 20.1788, 736.8517, 576.6822),
-    c(140.7113, 20.1788, 737.6445, 576.6822),
-    c(140.7113, 20.1788, 739.23, 575.8895),
-    c(140.7113, 19.3861, 364.264, 575.8895)
-  ),
-  list(
-    event_name = "Fort William",
-    event_type = "World Cup",
-    event_year = "2024",
-    round_type = "Timed Training",
-    round_category = "Men Elite"
-  ),
-  "inst/extdata/20240518-biel_dhi_me_results_tt.pdf",
-  list(
-    c(139.9186, 20.5752, 739.23, 575.4931),
-    c(140.7113, 19.7825, 738.4372, 576.2859),
-    c(140.7113, 19.7825, 738.4372, 576.2859),
-    c(140.7113, 19.7825, 737.6445, 576.2859),
-    c(140.7113, 19.7825, 738.4372, 576.2859),
-    c(140.7113, 19.7825, 739.23, 575.4931),
-    c(140.7113, 19.7825, 362.6785, 575.4931)
-  ),
-  list(
-    event_name = "Bielsko-Biala",
-    event_type = "World Cup",
-    event_year = "2024",
-    round_type = "Timed Training",
-    round_category = "Men Elite"
-  ),
-  "inst/extdata/20240607-leog_dhi_me_results_tt.pdf",
-  list(
-    c(141.5041, 19.7825, 739.23, 577.0786),
-    c(140.7113, 19.7825, 738.4372, 575.4931),
-    c(140.7113, 19.7825, 737.6445, 577.0786),
-    c(140.7113, 20.5752, 739.23, 577.8713),
-    c(139.9186, 20.5752, 738.4372, 576.2859),
-    c(140.7113, 19.7825, 739.23, 576.2859),
-    c(140.7113, 19.7825, 738.4372, 575.4931),
-    c(139.9186, 19.7825, 738.4372, 576.2859),
-    c(140.7113, 19.7825, 203.3378, 576.2859)
-  ),
-  list(
-    event_name = "Leogang",
-    event_type = "World Cup",
-    event_year = "2024",
-    round_type = "Timed Training",
-    round_category = "Men Elite"
-  ),
-  "inst/extdata/20240614-vdso_dhi_me_results_tt.pdf",
-  list(
-    c(140.7113, 19.7825, 737.6445, 575.4931),
-    c(140.7113, 19.7825, 738.4372, 576.2859),
-    c(140.7113, 19.7825, 737.6445, 575.4931),
-    c(140.7113, 19.7825, 738.4372, 577.0786),
-    c(140.7113, 19.7825, 736.059, 576.2859),
-    c(139.9186, 19.7825, 738.4372, 574.7004),
-    c(140.7113, 19.7825, 737.6445, 575.4931),
-    c(140.7113, 19.7825, 232.6692, 576.2859)
-  ),
-  list(
-    event_name = "Val di Sole",
-    event_type = "World Cup",
-    event_year = "2024",
-    round_type = "Timed Training",
-    round_category = "Men Elite"
-  ),
-  "inst/extdata/20240704-gets_dhi_me_results_tt.pdf",
-  list(
-    c(140.7113, 19.7825, 736.8517, 576.2859),
-    c(140.7113, 19.7825, 738.4372, 575.4931),
-    c(139.9186, 18.9897, 738.4372, 576.2859),
-    c(139.9186, 19.7825, 737.6445, 576.2859),
-    c(140.7113, 19.7825, 738.4372, 576.2859),
-    c(139.9186, 19.7825, 737.6445, 577.0786),
-    c(140.7113, 19.7825, 736.8517, 577.0786),
-    c(139.9186, 19.7825, 337.3108, 575.4931)
-  ),
-  list(
-    event_name = "Les Gets",
-    event_type = "World Cup",
-    event_year = "2024",
-    round_type = "Timed Training",
-    round_category = "Men Elite"
-  ),
-  "inst/extdata/20241004-mtsa_dhi_me_results_tt.pdf",
-  list(
-    c(140.7113, 19.7825, 739.23, 577.0786),
-    c(140.7113, 19.7825, 737.6445, 576.2859),
-    c(140.7113, 19.7825, 737.6445, 577.0786),
-    c(140.7113, 19.7825, 738.4372, 576.2859),
-    c(140.7113, 19.7825, 738.4372, 577.0786)
-  ),
-  list(
-    event_name = "Mont-Sainte-Anne",
-    event_type = "World Cup",
-    event_year = "2024",
-    round_type = "Timed Training",
-    round_category = "Men Elite"
-  )
+# ---- Extract PDF's ----
+results <- pmap(
+  .l = spec,
+  .f = \(path, areas, metadata) {
+    extract_tables(
+      file = path,
+      area = areas,
+      guess = FALSE,
+      method = "stream",
+      output = "tibble"
+    ) |>
+      bind_tables() |>
+      clean_results() |>
+      mutate(!!!metadata)
+  }
 )
 
-# ---- Map over PDFs ----
+world_cup_24_elite_men_results <- list_rbind(results)
 
-# ------------------------------------------------------------------------------
-# TODO:
-# - Iterate over paths and areas to extract, clean, combine, and save results
-# - Create one data set per category (e.g., elite men, elite women, etc.), and
-#   split the data sets by "timed training" and "results". All races for the
-#   whole season for one category should exist in a single dataframe.
-# - Delete old "world-cup-24-fort-william-elites.R" file and .rda files.
-# - Updae data.R with new changes and data sets.
-# ------------------------------------------------------------------------------
+usethis::use_data(world_cup_24_elite_men_results, overwrite = TRUE)

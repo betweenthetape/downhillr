7f10f6ebe35a96e1346cfddcea4f977826de9330
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2024-12-13 18:13:39 +0100

    Split results and training specs

--- data-raw/world-cup-24-elite-men-results.R
+++ data-raw/world-cup-24-elite-men-results.R
@@ -11,7 +11,7 @@ pkgload::load_all()
 
 # ---- Create spec ----
 # Run `pdf_areas(<INSERT PATH>)` on each path to determine PDF table areas.
-spec <- tribble(
+results_spec <- tribble(
   ~path, ~areas, ~metadata,
   "inst/extdata/20240503-fwil_dhi_me_results_f.pdf",
   list(
@@ -62,23 +62,6 @@ spec <- tribble(
     metadata_distance_km = 3,
     metadata_average_speed_kmh = 42.368
   ),
-  "inst/extdata/20240503-fwil_dhi_me_results_tt.pdf",
-  list(
-    c(140.7113, 20.1788, 738.4372, 576.6822),
-    c(140.7113, 19.3861, 736.8517, 575.8895),
-    c(140.7113, 20.1788, 738.4372, 575.8895),
-    c(140.7113, 20.1788, 736.8517, 576.6822),
-    c(140.7113, 20.1788, 737.6445, 576.6822),
-    c(140.7113, 20.1788, 739.23, 575.8895),
-    c(140.7113, 19.3861, 364.264, 575.8895)
-  ),
-  list(
-    event_name = "Fort William",
-    event_type = "World Cup",
-    event_year = "2024",
-    round_type = "Timed Training",
-    round_category = "Men Elite"
-  ),
   "inst/extdata/20240518-biel_dhi_me_results_f.pdf",
   list(
     c(133.5767, 19.7825, 679.7745, 575.4931)
@@ -128,23 +111,6 @@ spec <- tribble(
     metadata_temp_deg_c = 21,
     metadata_distance_km = 2.010,
     metadata_average_speed_kmh = 40.230
-  ),
-  "inst/extdata/20240518-biel_dhi_me_results_tt.pdf",
-  list(
-    c(139.9186, 20.5752, 739.23, 575.4931),
-    c(140.7113, 19.7825, 738.4372, 576.2859),
-    c(140.7113, 19.7825, 738.4372, 576.2859),
-    c(140.7113, 19.7825, 737.6445, 576.2859),
-    c(140.7113, 19.7825, 738.4372, 576.2859),
-    c(140.7113, 19.7825, 739.23, 575.4931),
-    c(140.7113, 19.7825, 362.6785, 575.4931)
-  ),
-  list(
-    event_name = "Bielsko-Biala",
-    event_type = "World Cup",
-    event_year = "2024",
-    round_type = "Timed Training",
-    round_category = "Men Elite"
   ),
   "inst/extdata/20240607-leog_dhi_me_results_f.pdf",
   list(
@@ -197,25 +163,6 @@ spec <- tribble(
     metadata_distance_km = 2.115,
     metadata_average_speed_kmh = 41.124
   ),
-  "inst/extdata/20240607-leog_dhi_me_results_tt.pdf",
-  list(
-    c(141.5041, 19.7825, 739.23, 577.0786),
-    c(140.7113, 19.7825, 738.4372, 575.4931),
-    c(140.7113, 19.7825, 737.6445, 577.0786),
-    c(140.7113, 20.5752, 739.23, 577.8713),
-    c(139.9186, 20.5752, 738.4372, 576.2859),
-    c(140.7113, 19.7825, 739.23, 576.2859),
-    c(140.7113, 19.7825, 738.4372, 575.4931),
-    c(139.9186, 19.7825, 738.4372, 576.2859),
-    c(140.7113, 19.7825, 203.3378, 576.2859)
-  ),
-  list(
-    event_name = "Leogang",
-    event_type = "World Cup",
-    event_year = "2024",
-    round_type = "Timed Training",
-    round_category = "Men Elite"
-  ),
   "inst/extdata/20240614-vdso_dhi_me_results_f.pdf",
   list(
     c(133.5767, 19.7825, 648.8576, 574.7004)
@@ -266,24 +213,6 @@ spec <- tribble(
     metadata_distance_km = 2.261,
     metadata_average_speed_kmh = 36.287
   ),
-  "inst/extdata/20240614-vdso_dhi_me_results_tt.pdf",
-  list(
-    c(140.7113, 19.7825, 737.6445, 575.4931),
-    c(140.7113, 19.7825, 738.4372, 576.2859),
-    c(140.7113, 19.7825, 737.6445, 575.4931),
-    c(140.7113, 19.7825, 738.4372, 577.0786),
-    c(140.7113, 19.7825, 736.059, 576.2859),
-    c(139.9186, 19.7825, 738.4372, 574.7004),
-    c(140.7113, 19.7825, 737.6445, 575.4931),
-    c(140.7113, 19.7825, 232.6692, 576.2859)
-  ),
-  list(
-    event_name = "Val di Sole",
-    event_type = "World Cup",
-    event_year = "2024",
-    round_type = "Timed Training",
-    round_category = "Men Elite"
-  ),
   "inst/extdata/20240704-gets_dhi_me_results_f.pdf",
   list(
     c(133.5767, 20.5752, 634.5883, 576.2859)
@@ -333,24 +262,6 @@ spec <- tribble(
     metadata_temp_deg_c = 20,
     metadata_distance_km = 2.380,
     metadata_average_speed_kmh = 41.934
-  ),
-  "inst/extdata/20240704-gets_dhi_me_results_tt.pdf",
-  list(
-    c(140.7113, 19.7825, 736.8517, 576.2859),
-    c(140.7113, 19.7825, 738.4372, 575.4931),
-    c(139.9186, 18.9897, 738.4372, 576.2859),
-    c(139.9186, 19.7825, 737.6445, 576.2859),
-    c(140.7113, 19.7825, 738.4372, 576.2859),
-    c(139.9186, 19.7825, 737.6445, 577.0786),
-    c(140.7113, 19.7825, 736.8517, 577.0786),
-    c(139.9186, 19.7825, 337.3108, 575.4931)
-  ),
-  list(
-    event_name = "Les Gets",
-    event_type = "World Cup",
-    event_year = "2024",
-    round_type = "Timed Training",
-    round_category = "Men Elite"
   ),
   "inst/extdata/20240906-loud_dhi_me_results_f.pdf",
   list(
@@ -433,6 +344,99 @@ spec <- tribble(
     metadata_temp_deg_c = 20,
     metadata_distance_km = 2.500,
     metadata_average_speed_kmh = 40.505
+  )
+)
+
+timed_training_spec <- tribble(
+  ~path, ~areas, ~metadata,
+  "inst/extdata/20240503-fwil_dhi_me_results_tt.pdf",
+  list(
+    c(140.7113, 20.1788, 738.4372, 576.6822),
+    c(140.7113, 19.3861, 736.8517, 575.8895),
+    c(140.7113, 20.1788, 738.4372, 575.8895),
+    c(140.7113, 20.1788, 736.8517, 576.6822),
+    c(140.7113, 20.1788, 737.6445, 576.6822),
+    c(140.7113, 20.1788, 739.23, 575.8895),
+    c(140.7113, 19.3861, 364.264, 575.8895)
+  ),
+  list(
+    event_name = "Fort William",
+    event_type = "World Cup",
+    event_year = "2024",
+    round_type = "Timed Training",
+    round_category = "Men Elite"
+  ),
+  "inst/extdata/20240518-biel_dhi_me_results_tt.pdf",
+  list(
+    c(139.9186, 20.5752, 739.23, 575.4931),
+    c(140.7113, 19.7825, 738.4372, 576.2859),
+    c(140.7113, 19.7825, 738.4372, 576.2859),
+    c(140.7113, 19.7825, 737.6445, 576.2859),
+    c(140.7113, 19.7825, 738.4372, 576.2859),
+    c(140.7113, 19.7825, 739.23, 575.4931),
+    c(140.7113, 19.7825, 362.6785, 575.4931)
+  ),
+  list(
+    event_name = "Bielsko-Biala",
+    event_type = "World Cup",
+    event_year = "2024",
+    round_type = "Timed Training",
+    round_category = "Men Elite"
+  ),
+  "inst/extdata/20240607-leog_dhi_me_results_tt.pdf",
+  list(
+    c(141.5041, 19.7825, 739.23, 577.0786),
+    c(140.7113, 19.7825, 738.4372, 575.4931),
+    c(140.7113, 19.7825, 737.6445, 577.0786),
+    c(140.7113, 20.5752, 739.23, 577.8713),
+    c(139.9186, 20.5752, 738.4372, 576.2859),
+    c(140.7113, 19.7825, 739.23, 576.2859),
+    c(140.7113, 19.7825, 738.4372, 575.4931),
+    c(139.9186, 19.7825, 738.4372, 576.2859),
+    c(140.7113, 19.7825, 203.3378, 576.2859)
+  ),
+  list(
+    event_name = "Leogang",
+    event_type = "World Cup",
+    event_year = "2024",
+    round_type = "Timed Training",
+    round_category = "Men Elite"
+  ),
+  "inst/extdata/20240614-vdso_dhi_me_results_tt.pdf",
+  list(
+    c(140.7113, 19.7825, 737.6445, 575.4931),
+    c(140.7113, 19.7825, 738.4372, 576.2859),
+    c(140.7113, 19.7825, 737.6445, 575.4931),
+    c(140.7113, 19.7825, 738.4372, 577.0786),
+    c(140.7113, 19.7825, 736.059, 576.2859),
+    c(139.9186, 19.7825, 738.4372, 574.7004),
+    c(140.7113, 19.7825, 737.6445, 575.4931),
+    c(140.7113, 19.7825, 232.6692, 576.2859)
+  ),
+  list(
+    event_name = "Val di Sole",
+    event_type = "World Cup",
+    event_year = "2024",
+    round_type = "Timed Training",
+    round_category = "Men Elite"
+  ),
+  "inst/extdata/20240704-gets_dhi_me_results_tt.pdf",
+  list(
+    c(140.7113, 19.7825, 736.8517, 576.2859),
+    c(140.7113, 19.7825, 738.4372, 575.4931),
+    c(139.9186, 18.9897, 738.4372, 576.2859),
+    c(139.9186, 19.7825, 737.6445, 576.2859),
+    c(140.7113, 19.7825, 738.4372, 576.2859),
+    c(139.9186, 19.7825, 737.6445, 577.0786),
+    c(140.7113, 19.7825, 736.8517, 577.0786),
+    c(139.9186, 19.7825, 337.3108, 575.4931)
+  ),
+  list(
+    event_name = "Les Gets",
+    event_type = "World Cup",
+    event_year = "2024",
+    round_type = "Timed Training",
+    round_category = "Men Elite"
   ),
   "inst/extdata/20241004-mtsa_dhi_me_results_tt.pdf",
   list(
@@ -451,6 +455,7 @@ spec <- tribble(
   )
 )
 
+# ---- Map over PDFs ----
 
 # ------------------------------------------------------------------------------
 # TODO:

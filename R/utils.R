496801a1ca022ee13fcae1c19f9afe78eead7dc4
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2024-12-13 21:10:52 +0100

    Remove `::` calls from imported tidyverse funs

--- R/utils.R
+++ R/utils.R
@@ -12,11 +12,11 @@ bind_tables <- function(tables) {
 #' @examples
 #' convert_to_seconds(c("3:14.668", "14.590"))
 convert_to_seconds <- function(x) {
-  purrr::map_dbl(
+  map_dbl(
     x,
     .f = \(x) {
       if (grepl(":", x)) {
-        parts <- stringr::str_split_1(x, ":")
+        parts <- str_split_1(x, ":")
         minutes <- as.numeric(parts[1])
         seconds <- as.numeric(parts[2])
         (minutes * 60) + seconds
@@ -39,7 +39,7 @@ convert_to_seconds <- function(x) {
 #' @noRd
 pdf_areas <- function(pdf, clipboard = TRUE) {
   output <- tabulapdf::locate_areas(pdf) |>
-    purrr::map(\(x) round(unname(x), 4))
+    map(\(x) round(unname(x), 4))
 
   print(dput(output))
 

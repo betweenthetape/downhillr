6cc1baac5f874ad1eea1036e55931003d65d4097
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2025-02-14 13:35:04 +0100

    Fix errors caused by integration of timed training data: - data entry errors - mis-matched image path names

--- inst/scripts/simulated-season.R
+++ inst/scripts/simulated-season.R
@@ -48,12 +48,12 @@ timed_training <- world_cup_24_elite_men_timed_training |>
   ) |>
   relocate(event_name, round_type, .after = time)
 
-# Correct data entry error for Kimi Viardot (hours are mistaken as minutes in
-# the raw data)
+# Correct data entry errors for Kimi Viardot & Valentin Rohrmoser (in both
+# cases, hours are mistaken as minutes).
 timed_training <- timed_training |>
   mutate(
     across(
-      c(split_3, split_4),
+      c(split_3, split_4, time),
       ~if_else(
         name == "Kimi Viardot" &
           event_name == "Bielsko-Biala" &
@@ -61,7 +61,27 @@ timed_training <- timed_training |>
         NA,
         .x
       )
+    ),
+    across(
+      c(split_3, split_4, time),
+      ~if_else(
+        name == "Valentin Rohrmoser" &
+          event_name == "Val di Sole" &
+          round_type == "Timed Training 1",
+        NA,
+        .x
+      )
     )
+  )
+
+# Check that later splits are always larger than earlier splits to catch any
+# other data entry errors
+timed_training <- timed_training |>
+  mutate(
+    split_2 = if_else(split_2 <= split_1, NA, split_2),
+    split_3 = if_else(split_3 <= split_2, NA, split_3),
+    split_4 = if_else(split_4 <= split_3, NA, split_4),
+    time = if_else(time <= split_4, NA, time)
   )
 
 image_data <- tibble(path = dir_ls("inst/rider-images")) |>
@@ -74,6 +94,7 @@ image_data <- tibble(path = dir_ls("inst/rider-images")) |>
       name == "Lachlan StevensMcNab" ~ "Lachlan Stevens-Mcnab",
       name == "Oisin OCallaghan" ~ "Oisin Callaghan O",
       name == "Remi Thirion" ~ "Rémi Thirion",
+      name == "Ethan Craik" ~ "Ethan George Craik",
       .default = name
     )
   )

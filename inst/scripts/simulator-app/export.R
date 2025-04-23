9293cc93b44a88ba641126133b2b8af828f9cd21
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2025-04-23 15:30:17 +0200

    Export all sectors table for simulator app

---
+++ inst/scripts/simulator-app/export.R
@@ -0,0 +1,74 @@
+# ------------------------------------------------------------------------------
+# Load
+# ------------------------------------------------------------------------------
+pkgload::load_all()
+library(tidyverse)
+
+# ------------------------------------------------------------------------------
+# Prepare data sets
+# ------------------------------------------------------------------------------
+results <- world_cup_24_elite_men_results |>
+  mutate(name = str_to_title(name)) |>
+  mutate(
+    name = map_chr(str_split(name, " "), ~ str_c(rev(.x), collapse = " "))
+  ) |>
+  mutate(across(everything(), ~ if_else(is.infinite(.x), NA, .x)))
+
+timed_training <- world_cup_24_elite_men_timed_training |>
+  mutate(name = str_to_title(name)) |>
+  mutate(
+    name = map_chr(str_split(name, " "), ~ str_c(rev(.x), collapse = " "))
+  ) |>
+  mutate(across(everything(), ~ if_else(is.infinite(.x), NA, .x))) |>
+  select(
+    name,
+    contains("_split_"),
+    matches("\\d+_time$"),
+    event_name,
+    round_type
+  ) |>
+  pivot_longer(
+    cols = matches("^(run_\\d+_split_\\d+|run_\\d+_time)$"),
+    names_to = c("run_number", "split_type"),
+    names_pattern = "run_(\\d+)_(.+)",
+    values_to = "value"
+  ) |>
+  mutate(round_type = paste(round_type, run_number)) |>
+  select(-run_number) |>
+  pivot_wider(
+    names_from = split_type,
+    values_from = value
+  ) |>
+  relocate(event_name, round_type, .after = time)
+
+# ------------------------------------------------------------------------------
+# All sectors table
+# ------------------------------------------------------------------------------
+all_sectors <- results |>
+  select(name, starts_with("split"), time, event_name, round_type) |>
+  bind_rows(timed_training) |>
+  mutate(
+    `1` = split_1,
+    `2` = split_2 - split_1,
+    `3` = split_3 - split_2,
+    `4` = split_4 - split_3,
+    `5` = time - split_4
+  ) |>
+  select(-starts_with("split_")) |>
+  mutate(
+    round_type = factor(
+      round_type,
+      levels = c(
+        "Timed Training 1",
+        "Timed Training 2",
+        "Timed Training 3",
+        "Qualifying",
+        "Semi-Final",
+        "Final"
+      )
+    )
+  ) |>
+  relocate(time, .after = `5`) |>
+  arrange(round_type)
+
+write_csv(all_sectors, "all_sectors.csv")

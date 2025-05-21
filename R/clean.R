64af182c54f3c8ab5dbfdc0cb08d6c470046d5bb
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2025-05-21 15:54:01 +0200

    Add 25 cleaning funs

--- R/clean.R
+++ R/clean.R
@@ -78,6 +78,83 @@ clean_results_24 <- function(data) {
     )
 }
 
+#' Clean a results table extracted from a PDF for the 2025 season
+#'
+#' Use this function to clean qualifying, semi-final, and final result tables
+#' after they have been extracted from a PDF and comined into a single table in
+#' their raw format.
+#'
+#' @param data A data frame containing all combined tables from a PDF in their
+#'   raw, unprocessed form.
+#' @return A tibble with cleaned results.
+#' @noRd
+#' @autoglobal
+clean_results_25 <- function(data) {
+  data <- data |>
+    fill(`UCI ID`)
+
+  odd_rows <- data |>
+    filter(row_number() %% 2 == 1) |>
+    separate_wider_regex(
+      cols = `Nr Name / UCI MTB Team`,
+      patterns = c(Nr = "^\\d+", `Name / UCI MTB Team` = "\\D+")
+    ) |>
+    janitor::clean_names() |>
+    rename(
+      name = name_uci_mtb_team,
+      split_1 = i1_i2,
+      split_3 = i3_i4
+    ) |>
+    mutate(name = str_remove(name, "\\*$")) |>
+    mutate(name = str_squish(name))
+
+  even_rows <- data |>
+    filter(row_number() %% 2 == 0) |>
+    select(
+      uci_team = `Nr Name / UCI MTB Team`,
+      uci_id = `UCI ID`,
+      split_2 = `I1 / I2`,
+      split_4 = `I3 / I4`,
+      time_from_leader = Time
+    )
+
+  odd_rows |>
+    left_join(even_rows) |>
+    mutate(
+      dnf = if_else(time == "DNF", TRUE, FALSE),
+      dsq = if_else(time == "DSQ", TRUE, FALSE),
+      dns = if_else(time == "DNS", TRUE, FALSE),
+      across(everything(), ~ if_else(.x == "-", NA, .x)),
+      across(starts_with("sp"), ~ str_remove(.x, "\\s*\\(\\d+\\)$")),
+      time_from_leader = str_remove(time_from_leader, "^\\+"),
+      across(
+        c(starts_with("split_"), starts_with("time")),
+        ~ convert_to_seconds(.x)
+      ),
+      across(c(rank, nr, uci_id, speed, points), ~ as.numeric(.x))
+    ) |>
+    select(
+      rank,
+      nr,
+      name,
+      uci_team,
+      uci_id,
+      nat,
+      yob,
+      speed,
+      split_1,
+      split_2,
+      split_3,
+      split_4,
+      time,
+      time_from_leader,
+      dnf,
+      dsq,
+      dns,
+      points
+    )
+}
+
 #' Clean timed training results extracted from a PDF for the 2024 season
 #'
 #' Use this function to clean timed training results tables after they have been
